3c3abc25693039dd325f479c9dda00ea18530572
Cameron <cameroningratta@hotmail.com>
2023-06-27 16:36:02 -0400

    Started styling components with styled-components library

--- src/Components/CenterStuff/MinimalTemperature.jsx
+++ src/Components/CenterStuff/MinimalTemperature.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+
+const Temp = styled.h1`
+  font-size: 30px;
+  margin: 0;
+`;
+const Container = styled.div`
+  position: relative;
+  background-color: red;
+  align-items: center;
+`;
+const HighLow = styled.h4`
+  margin: 0;
+  font-size: 10px;
+`;
 
 const MinimalTemperature = () => {
   return (
-    <div>
-      <h1>Temp</h1>
-      <div>
-        <h4>high/low</h4>
-      </div>
-    </div>
+    <Container>
+      <Temp>30</Temp>
+      <HighLow>high/low</HighLow>
+    </Container>
   );
 };
 export default MinimalTemperature;
